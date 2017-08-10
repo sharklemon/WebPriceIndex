d399b18e245b04425b22c253deb2c8eda0c26a1b
j-webber <Jwebbie@Jamess-MacBook-Pro.local>
2017-08-10 14:56:13 -0400

    AJAX calls added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,6 +23,64 @@ var Wcharger=0;
 var Wdetergent=0;
 var Wbasket = Wcoffee + Wswiffer + Wlotion + Wcharger + Wdetergent;
 
+
+// AJAXes; note, there seems to be an issue with how some of these retailers handle these requests, I had to install an extension on Chrome for it to work properly. No big deal I guess, but it might cause some issues long term? Dunno. -jw
+$.ajax({
+	url: "http://api.walmartlabs.com/v1/items/49670830?format=json&apiKey=v9whmb7spr426g5qv8sd2j4k",
+	method: "GET"
+
+}).done(function(response){
+
+	Wcoffee = response.salePrice;
+	console.log("Coffee costs: " + Wcoffee);
+
+})
+
+$.ajax({
+	url: "http://api.walmartlabs.com/v1/items/11045958?format=json&apiKey=v9whmb7spr426g5qv8sd2j4k",
+	method: "GET"
+
+}).done(function(response){
+
+	Wswiffer = response.salePrice;
+
+	console.log("Swiffer costs: " + Wswiffer);
+});
+
+$.ajax({
+	url: "http://api.walmartlabs.com/v1/items/10818473?format=json&apiKey=v9whmb7spr426g5qv8sd2j4k",
+	method: "GET"
+
+}).done(function(response){
+
+	Wlotion = response.salePrice;
+
+	console.log("Lotion costs: " + Wlotion);
+});
+
+$.ajax({
+	url: "http://api.walmartlabs.com/v1/items/45820988?format=json&apiKey=v9whmb7spr426g5qv8sd2j4k",
+	method: "GET"
+
+}).done(function(response){
+
+	Wcharger = response.salePrice;
+
+	console.log("Charger costs: " + Wcharger);
+});
+
+$.ajax({
+	url: "http://api.walmartlabs.com/v1/items/723835749?format=json&apiKey=v9whmb7spr426g5qv8sd2j4k",
+	method: "GET"
+
+}).done(function(response){
+
+	Wdetergent = response.salePrice;
+
+	console.log("Pods cost: " + Wdetergent);
+
+});
+
 $("#price-check").on("click", priceUpdate)
 
 
@@ -35,7 +93,7 @@ function priceUpdate(){
         	lotion: Alotion,
         	charger: Acharger,
       	});
-	database.ref().child("/Walmart")
+	database.ref().child("/Walmart").set({
 			coffee: Wcoffee,
         	swiffer: Wswiffer,
         	lotion: Wlotion,
