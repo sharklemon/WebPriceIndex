175c8e1c16311595eaa7f6cdddf5d52124615e69
sharklemon <anastasia.l.mark@gmail.com>
2017-08-10 11:47:52 -0400

    first round of HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyDhOOiR8Xc9MeOPN73Qym3k2yWyH0V_elk",
+    authDomain: "webpriceindex.firebaseapp.com",
+    databaseURL: "https://webpriceindex.firebaseio.com",
+    projectId: "webpriceindex",
+    storageBucket: "webpriceindex.appspot.com",
+    messagingSenderId: "48951022685"
+  };
+  firebase.initializeApp(config);
